a8ea43f269e9c9ae7b00ebb0c0b94ae4204cf018
elliotyou <m9402311@mail.ntust.edu.tw>
2021-09-03 17:32:38 +0800

    not important. Just tried to comment what I've known with the syntax

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 // require module as reader
-const { raw } = require('express')
 const reader = require('xlsx')
+const file = reader.readFile('./test.xlsx', { bookSheets: false })
+
+function get_header_row(sheet) {
+  var headers = [];
+  var range = reader.utils.decode_range(sheet['!ref']);
+  console.log('range', range)
+  var i, j = range.s.r; /* start in the first row */
+  /* walk every column in the range */
+  for (i = range.s.c; i <= range.e.c; ++i) {   //s: start, e: end, c: column, r: row
+    /* find the cell in the first row */
+    var cell = sheet[reader.utils.encode_cell({ c: i, r: j })]    //{c:i, r:j} 是坐標，例如{c:1, r:3} 就是第 2 欄第 4 列
+    console.log(`cell-${i}`)
+    console.log(cell)
+    var header = "UNKNOWN " + i;      //設定 column name 的預設值
+    //cell 的 property 可參考: https://docs.sheetjs.com/
+    //cell.t 是 type，就是那1格是數字、文字、日期…。
+    if (cell && cell.t) {    //當所爬到的那格是有意義的時候
+      header = reader.utils.format_cell(cell);   //format_cell: generates the text value for a cell(using number formats).
+    }
+    headers.push(header);
+  }
+  return headers;
+}
+
+console.log(get_header_row(file.Sheets[file.SheetNames[0]]))
+//================================================
 
 //read xlsx file as file
-const file = reader.readFile('./test.xlsx', { sheetRows: 3 })
+//for parsing options, check https://github.com/SheetJS/sheetjs#parsing-options
+const file = reader.readFile('./test.xlsx', { bookSheets: false })
 
 let data = []
 
@@ -18,8 +44,12 @@ for (let i = 0; i < sheets.length; i++) {
   temp.forEach((res) => { data.push(res) })
 }
 
+//================================================
+
 // Printing data
-console.log(data)
+// console.log(data)
+console.log(get_header_row(file.Sheets[file.SheetNames[0]]))
+
 
 // // Sample data set
 // let student_data = [{
@@ -40,3 +70,6 @@ console.log(data)
 // reader.utils.book_append_sheet(file, ws, "Sheet3")
 
 // reader.writeFile(file, './test.xlsx')
+
+
+
